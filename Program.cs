7dfd7dfb5bdb8a83e8e193b0320dbb793a2fecdf
mahendran <mahe.m@hotmail.com>
2017-05-30 15:02:05 +0530

    d
    d

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace coreweb
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
-                .UseApplicationInsights()
+                .UseApplicationInsights()                
                 .Build();
 
             host.Run();
